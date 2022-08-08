8eab1c3da4d8aa9766b67a3da5be9270c6abd0c3
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-08 14:50:36 -0700

    changing env file, adding cookieParser, adding route to /api/v1/secrets in app.js and setting up secrets table in postgres, database connected successfully

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,8 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS secrets;
+
+CREATE TABLE secrets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT,
+    description TEXT, 
+    created_at TIMESTAMP
+);
