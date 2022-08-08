50894cc9ed41edf647e2bb024e0e7b7077cf48c1
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-08 15:01:07 -0700

    adding swl table for users

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,13 +4,29 @@ const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
 
+//dummy user for testing purposes
+const testUser = {
+  title: 'Test',
+  description: 'Test Secret',
+};
+
+const regiserAndLogin = async (userProps = {})=> {
+  const title = userProps.title ?? testUser.title;
+  //create an "agent" that gives us the ability to store cookies between requests in a test
+  const agent =
+   
+}
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
   it('example test - delete me!', () => {
     expect(1).toEqual(1);
   });
+
+
   afterAll(() => {
     pool.end();
   });
