6410d6b6053717b7913a2fce5a615bae8bd74f88
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-08 15:07:29 -0700

    defining testUser and writing creates a new user test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,18 +6,27 @@ const UserService = require('../lib/services/UserService');
 
 //dummy user for testing purposes
 const testUser = {
-  title: 'Test',
-  description: 'Test Secret',
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@test.com',
+  password: 'testing',
 };
 
-const regiserAndLogin = async (userProps = {})=> {
-  const title = userProps.title ?? testUser.title;
+const registerAndLogin = async (userProps = {})=> {
+  const password = userProps.password ?? testUser.password;
   //create an "agent" that gives us the ability to store cookies between requests in a test
-  const agent =
-   
-}
+  const agent = request.agent(app);
+  
+  //create a user to sign in with 
+  const user = await UserService.create({ ...testUser, ...userProps });
 
-describe('backend-express-template routes', () => {
+  //then sign in 
+  const { email } = user;
+  await (await agent.post('./api/v1/secrets/sessions')).setEncoding({ email, password });
+  return [agent, user];
+};
+
+describe('top-secrets routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -26,8 +35,19 @@ describe('backend-express-template routes', () => {
     expect(1).toEqual(1);
   });
 
-
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(testUser);
+    const { firstName, lastName, email } = testUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
 });
